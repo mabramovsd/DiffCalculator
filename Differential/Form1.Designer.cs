934a908d0c50bae9ed6106771e4125ee07203d3f
Mikhail Abramov <beavisabra@yandex.ru>
2019-04-09 10:12:38 +0300

    Перевод из инфиксной в постфиксную форму

---
+++ Differential/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Differential
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FunctionTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.DiffTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FunctionTextBox
+            // 
+            this.FunctionTextBox.Location = new System.Drawing.Point(14, 14);
+            this.FunctionTextBox.Margin = new System.Windows.Forms.Padding(5);
+            this.FunctionTextBox.Name = "FunctionTextBox";
+            this.FunctionTextBox.Size = new System.Drawing.Size(1068, 34);
+            this.FunctionTextBox.TabIndex = 0;
+            this.FunctionTextBox.Text = "96 - 3 * 5 ^ 2 + 5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1068, 123);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(14, 189);
+            this.ResultTextBox.Multiline = true;
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(341, 223);
+            this.ResultTextBox.TabIndex = 3;
+            // 
+            // DiffTextBox
+            // 
+            this.DiffTextBox.Location = new System.Drawing.Point(361, 189);
+            this.DiffTextBox.Multiline = true;
+            this.DiffTextBox.Name = "DiffTextBox";
+            this.DiffTextBox.Size = new System.Drawing.Size(721, 223);
+            this.DiffTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 419);
+            this.Controls.Add(this.DiffTextBox);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FunctionTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FunctionTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.TextBox DiffTextBox;
+    }
+}
+
